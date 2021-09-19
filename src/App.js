8f8917577d26c1529475e431347d73245412e3ea
seriko87 <58387052+seriko87@users.noreply.github.com>
2021-09-19 14:50:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import data from "./data";
 function App() {
+  const [value, setValue] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(value);
+    if (amount >= data.length) {
+      amount = data.length;
+    }
+    if (amount <= 0) {
+      amount = 1;
+    }
+    setText(data.slice(0, amount));
+
+    console.log(text);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>tired of boring lorem ipsum?</h1>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='number'>Pragraphs: </label>
+          <input
+            type='number'
+            name='number'
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button>Generate</button>
+        </form>
+      </div>
+
+      <div className='text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </div>
     </div>
   );
 }
